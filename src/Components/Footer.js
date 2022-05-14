dedc07c1458830252b192e2883074c72ae17721b
Samikshya K C <sam@Samikshyas-MacBook-Air.local>
2022-05-14 17:09:44 -0400

    deleted unnecessary files and added a footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../styles/Footer.css"
+export default function Footer(){
+    return(
+        <div className="Footer">
+            <p>This project was coded by <a href="https://samikshyasworld.com/">Samikshya K C</a> and is Open-sourced on Github.</p>
+        </div>
+    )
+}
