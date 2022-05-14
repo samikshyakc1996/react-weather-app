9cfc7acfd9e0c4b988936c14ec2e7dfe8ce18a75
Samikshya K C <sam@Samikshyas-MacBook-Air.local>
2022-05-14 17:17:26 -0400

    changed folder structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-
+import './styles/App.css';
+import Footer from './Components/Footer';
 function App() {
   return (
     <div className="App">
      <h1>Weather app</h1>
+     <Footer/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/Index.css';
 import App from './App';
-import Footer from './Components/Footer';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Footer/>
+    
   </React.StrictMode>
 );
